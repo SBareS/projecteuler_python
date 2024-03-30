36c69f626ffd028667333eee248c810ecf451ec3
sbares <1410678+SBareS@users.noreply.github.com>
2024-03-30 14:39:10 +0100

    Problem 356

--- matrix.py
+++ matrix.py
@@ -1,4 +1,7 @@
-"""Contains simple helpers for matrix algebra. For now just 2x2 matrices"""
+"""
+Contains simple helpers for matrix algebra.
+For now just 2x2 and 3x3 matrices.
+"""
 
 from functools import cache
 from typing import Tuple
@@ -43,4 +46,55 @@ def Mat22(t) -> type:
     Mat22t.__name__ = f"Mat22({t})"
     Mat22t.__qualname__ = ''.join([*Mat22t.__qualname__.split('.')[:-3], f'Mat22({t})'])
     return Mat22t
+
+@cache
+def Mat33(t) -> type:
+    class Mat33t(Tuple[t, t, t, t, t, t, t, t, t]):
+        def __new__(cls, a11, a12, a13, a21, a22, a23, a31, a32, a33):
+            return tuple.__new__(cls, (a11, a12, a13, a21, a22, a23, a31, a32, a33))
+        def __add__(self, other):
+            return Mat33t(*map(t.__add__, self, other))
+        def __radd__(self, other):
+            return Mat33t(*map(t.__radd__, self, other))
+        def __sub__(self, other):
+            return Mat33t(*map(t.__sub__, self, other))
+        def __rsub__(self, other):
+            return Mat33t(*map(t.__rsub__, self, other))
+        def __mul__(self, other):
+            a11, a12, a13, a21, a22, a23, a31, a32, a33 = self
+            b11, b12, b13, b21, b22, b23, b31, b32, b33 = other
+            return Mat33t(a11*b11 + a12*b21 + a13*b31, a11*b12 + a12*b22 + a13*b32, a11*b13 + a12*b23 + a13*b33,
+                          a21*b11 + a22*b21 + a23*b31, a21*b12 + a22*b22 + a23*b32, a21*b13 + a22*b23 + a23*b33,
+                          a31*b11 + a32*b21 + a33*b31, a31*b12 + a32*b22 + a33*b32, a31*b13 + a32*b23 + a33*b33)
+        def __rmul__(self, other):
+            a11, a12, a13, a21, a22, a23, a31, a32, a33 = other
+            b11, b12, b13, b21, b22, b23, b31, b32, b33 = self
+            return Mat33t(a11*b11 + a12*b21 + a13*b31, a11*b12 + a12*b22 + a13*b32, a11*b13 + a12*b23 + a13*b33,
+                          a21*b11 + a22*b21 + a23*b31, a21*b12 + a22*b22 + a23*b32, a21*b13 + a22*b23 + a23*b33,
+                          a31*b11 + a32*b21 + a33*b31, a31*b12 + a32*b22 + a33*b32, a31*b13 + a32*b23 + a33*b33)
+        def __pow__(self, n):
+            if n == 0:
+                return self.identity
+            if n < 0:
+                raise NotImplementedError
+            if n % 2 == 0:
+                square = self * self
+                return square.__pow__(n // 2)
+            return self * self.__pow__(n-1)
+        def __truediv__(self, other):
+            raise NotImplementedError
+        def __rtruediv__(self, other):
+            raise NotImplementedError
     
+    Mat33t.identity = Mat33t(1, 0, 0, 0, 1, 0, 0, 0, 1)
+    
+    Mat33t.__name__ = f"Mat33({t})"
+    Mat33t.__qualname__ = ''.join([*Mat33t.__qualname__.split('.')[:-3], f'Mat33({t})'])
+    return Mat33t
+
+# TODO: 
+#   Matrix-vector mult.
+#   Determinant
+#   Inverses
+#   Arbitrary-size matrices
+#   ...
---
+++ problem356.py
@@ -0,0 +1,35 @@
+r"""
+Largest Roots of Cubic Polynomials
+Let $a_n$ be the largest real root of a polynomial $x^3 - 2^n x^2 + n$.
+For example, $a_2 = 3.86619826...$ Find the last eight digits of
+\sum_{i=1}^{30} \floor{a_i^{987654321}}.
+
+Note:
+represents the floor function.
+"""
+
+from matrix import Mat33
+from mod_arith import ZMod
+
+
+N = 987654321
+themod = 10**8
+ZM = ZMod(themod)
+Mat = Mat33(ZM)
+
+# Let a, b, c be the three roots. Then f(k) = a**k + b**k + c**k satisfies
+# f(0) = 3
+# f(1) = 2**n
+# f(2) = (a + b + c)**2 - 2(a*b + b*c + c*d) = 2**(2*n)
+# f(k) = 2**n * f(k-1) - n*f(k-3)
+# Further, if a is the largest root, then 0 < b**N + c**N < 1 for N large, so
+# floor (a**N) = f(N) - 1.
+
+result = 0
+for i in range(1, 31):
+    themat = Mat(ZM(2)**i, ZM(0), -ZM(i),
+                 ZM(1), ZM(0), ZM(0),
+                 ZM(0), ZM(1), ZM(0)
+                 )**N
+    result += themat[6] * ZM(2)**(2*i) + themat[7] * ZM(2)**i + themat[8] * 3 - ZM(1)
+print(result)

870a837cb00087485a95b415a7e85d50900b23b3
sbares <1410678+SBareS@users.noreply.github.com>
2024-05-03 15:09:23 +0200

    Slight improvement to Problem 365

--- problem365.py
+++ problem365.py
@@ -10,15 +10,13 @@ Calculate $\sum M(10^{18}, 10^9, p \cdot q \cdot r)$ for
 """
 
 from itertools import combinations, dropwhile
-from time import perf_counter
 from mod_arith import binom_modp, crt
 from primes import primes_lt
 
-# TODO: make this less sloooow (currently ~2 minutes on my shitty 
-# laptop in CPython, and ~25 seconds in PyPy).
-
 primes = list(dropwhile(lambda p: p <= 1000, primes_lt(5000)))
 primes_and_binoms = [(p, binom_modp(10**18, 10**9, p)) for p in primes]
+zero_mod = [t for t in primes_and_binoms if t[1] == 0]
+nonzero_mod = [t for t in primes_and_binoms if t[1] != 0]
 
-print(sum(crt(x) for x in combinations(primes_and_binoms, 3)))
+print(sum(crt(x) for x in combinations(primes_and_binoms, 3) if any(t[1] != 0 for t in x)))
 correct_answer = "162619462356610313"

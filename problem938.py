bfd1e1288faf7a4a8504bae8ccacfa37501fe4a6
sbares <1410678+SBareS@users.noreply.github.com>
2025-07-10 18:04:24 +0200

    Improved Problem 938 to O(1)

--- problem938.py
+++ problem938.py
@@ -24,9 +24,19 @@ Find P(24690, 12345). Give your answer with 10 digits after the decimal point.
 # (credit to griff on the forum for noticing this. The formula is of course 
 # easy to derive once one has the idea.)
 
-result = 1/3 # = P(2, 1)
-for n in range(2, 12345 + 1):
-    result = (4 * (2*n - 1)**2 * result - 1)/(16 * n**2 - 16 * n + 3)
+# result = 1/3 # = P(2, 1)
+# for n in range(2, 12345 + 1):
+#     result = (4 * (2*n - 1)**2 * result - 1)/(16 * n**2 - 16 * n + 3)
+
+# The recursion even admits an exact solution in terms of the gamma function:
+# P(n) = 1 - (2**(2*n-1) * Gamma(n + 1/2))/(sqrt(pi) * Gamma(1/2 + 2*n)).
+# The huge intermediate values can be avoided by taging logs.
+
+from math import exp, lgamma, log, pi
+
+n = 12345
+
+result = 1 - exp((2*n-1)*log(2) + 2*lgamma(n + 0.5) - 0.5*log(pi) - lgamma(0.5 + 2*n))
 print(f"{result:.10f}")
 
 correct_answer = "0.2928967987"
